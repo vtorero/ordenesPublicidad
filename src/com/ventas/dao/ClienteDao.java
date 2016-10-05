b14f4078c8a9978331d6a21c378ffe5a7496cd67
Victor Jimenez <vjimenez@americatv.com.pe>
2016-10-05 18:15:49 -0500

    sis

--- src/com/ventas/dao/ClienteDao.java
+++ src/com/ventas/dao/ClienteDao.java
@@ -73,21 +73,23 @@ public List<Cliente> listar() throws Exception{
    }
             
                    
-public void modificar(Cliente cli) throws Exception{
+public int modificar(String razon,String contacto,String rpt_legal,String dni,String rpt_direccion,String ruc ,String direccion,String telefono,int codigo) throws Exception{
+     int numFa = 0;
        try {
+           
        this.Conectar();
            PreparedStatement st = this.getCn().prepareStatement("UPDATE ord_clientes SET RAZON_SOCIAL=?,CONTACTO=?,RPT_LEGAL=?,RPT_DNI=?,RPT_DIRECCION=?,RUC=?" +
             ",DIRECCION=?,TELEFONO=? WHERE C_CLIENTE = ?");
-           st.setString(1, cli.getRazonsocial());
-           st.setString(2,cli.getContacto());
-           st.setString(3, cli.getRptlegal());
-           st.setString(4, cli.getRptdni());
-           st.setString(5, cli.getRptdireccion());
-           st.setString(6, cli.getRuc());
-           st.setString(7, cli.getDireccion());
-           st.setString(8, cli.getTelefono());
-           st.setString(9,cli.getC_cliente());
-           st.executeUpdate();
+           st.setString(1, razon);
+           st.setString(2,contacto);
+           st.setString(3, rpt_legal);
+           st.setString(4, dni);
+           st.setString(5, rpt_direccion);
+           st.setString(6, ruc);
+           st.setString(6, direccion);
+           st.setString(7, telefono);
+           st.setInt(8,codigo);
+           numFa =st.executeUpdate();
        } catch (Exception e) {
    
         throw e;
@@ -95,7 +97,7 @@ public void modificar(Cliente cli) throws Exception{
            this.Cerrar();
         
        }
-   
+    return numFa;
    } 
            
 
